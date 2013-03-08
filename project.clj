c02b4e8a953da8ec735dfa17cdf3802e751095ab
Peter Schmiedeskamp <peter@thoughtspot.net>
2013-03-08 10:39:04 -0800

    Add EPL license to leiningen project def

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clj-hdf5 "0.2"
   :description "HDF5 interface for Clojure based on JHDF5"
   :url "https://github.com/clojure-numerics/clj-hdf5"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot [clj-hdf5.core]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.mikera/core.matrix "0.3.0"]
