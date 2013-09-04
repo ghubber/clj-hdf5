e37e1491bbf857e35bd02c620f1c8227a3bfd239
Peter Schmiedeskamp <peter@thoughtspot.net>
2013-09-04 16:04:52 -0700

    version bump jhdf5 dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-hdf5 "0.2"
+(defproject clj-hdf5 "0.2.1"
   :description "HDF5 interface for Clojure based on JHDF5"
   :url "https://github.com/clojure-numerics/clj-hdf5"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :aot [clj-hdf5.core]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [net.mikera/core.matrix "0.3.0"]
-                 [cisd/jhdf5 "12.02.3"]
+                 [cisd/jhdf5 "13.06.2"]
 		 ;Figure out what is needed from old contrib
                  ;[org.clojure/clojure-contrib "1.2.0"]
                  ]
