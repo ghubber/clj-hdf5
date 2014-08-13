6fe3d5429d069ef3fc52a230e1528250c34d03d2
hokkaido <hello@kimchi.io>
2014-08-13 20:46:07 +0200

    Fixed formatting issue in project.clj

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.mikera/core.matrix "0.28.0"]
                  [io.kimchi/cisd-jhdf5-core "13.06.2"]]
-  :native-dependencies [[io.kimchi/cisd-jhdf5-native-deps"13.06.2"]]
+  :native-dependencies [[io.kimchi/cisd-jhdf5-native-deps "13.06.2"]]
   :plugins [[codox "0.6.4"]])
