1d4203a0d8b914ffbe0b0e5356e99375a0d3cc6e
hokkaido <hello@kimchi.io>
2014-08-15 16:41:51 +0200

    Updated core_test.clj

--- test/clj_hdf5/core_test.clj
+++ test/clj_hdf5/core_test.clj
@@ -5,21 +5,50 @@
 
 (def tmp-dir (System/getProperty "java.io.tmpdir"))
 
+(defn get-tmp-file []
+  (io/file tmp-dir "file.h5"))
+
+(defmacro with-open
+  [bindings & body]
+  `(let [~(first bindings) (open ~(second bindings) ~(bindings 2))]
+      (try
+        ~@body
+        (finally
+          (close ~(first bindings))))))
+
+(defn delete-tmp-file []
+  (let [tmp-file get-tmp-file]
+  (if (.exists (tmp-file))
+    (io/delete-file (tmp-file)))))
+
+(defn setup-each []
+  (delete-tmp-file))
+ 
+(defn teardown-each []
+  (delete-tmp-file))
+
+(defn fixture-each [f]
+  (setup-each)
+  (f)
+  (teardown-each))
+
+(use-fixtures :each fixture-each)
+
+(deftest create-new-file-with-read-write
+  (testing "Opening a file with :read-write should create the 
+           file if it doesn't exist yet."
+    (let [h5file (get-tmp-file)]
+      (is (false? (.exists h5file)))
+          (with-open [h5root h5file :read-write]
+            (is (true? (.exists h5file)))
+            (is (true? (root? h5root)))))))
 
 
-(use-fixtures :each
-              (fn [f]
-                (if (.exists (io/file tmp-dir "file.h5"))
-                  (io/delete-file (io/file tmp-dir "file.h5")))
-                (f)
-                (io/delete-file (io/file tmp-dir "file.h5"))))
+(deftest create-a-new-group
+  (testing "Creating a new group"))
 
 
-(deftest create-file-with-read-write
-  (testing "create a file with open"
-    (let [h5file (io/file tmp-dir "file.h5")
-          h5root (open h5file :read-write)]
-      (.exists h5file))))
+
 
 
 
